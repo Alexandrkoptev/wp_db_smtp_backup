b0166cc81cedb59cf4d0aa52ff0c2ed545f21bee
Alexandrkoptev <alexandr.koptev@gmail.com>
2019-03-19 15:55:37 +0200

    cron+sqldump_sh

--- sqldump_mailsend.sh
+++ sqldump_mailsend.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-
+export destmail=alexandr.koptev@gmail.com
 export TIMESTAMP=$(date +'%Y%m%d')
 mysqldump $MYSQL_DATABASE -u$MYSQL_USER -hdb -p$MYSQL_PASSWORD 2>/dev/null > /sqldb_dump/dump$TIMESTAMP.sql
-#gzip -rmf /sqldb_dump/dump${TIMESTAMP}.sql > /sqldb_dump/db_dump.sql.gz
 gzip -rmf /sqldb_dump/dump${TIMESTAMP}.sql
-#mpack -s"" -d /sqldb_dump/db_dump.sql.gz alexandr.koptev@gmail.com
-mpack -s"" -d /sqldb_dump/dump*.gz alexandr.koptev@gmail.com
-#mv /sqldb_dump/db_dump.sql.gz /sqldb_dump/db_dump.sql-old.gz
+mpack -s"" -d /sqldb_dump/dump${TIMESTAMP}.sql.gz $destmail
 mv /sqldb_dump/dump*.gz /sqldb_dump/db_dump.sql-old.gz
 tar zcvf /sqldb_dump/wp-content${TIMESTAMP}.tar /wp-content
-sleep 10
-mpack -s"" -d /sqldb_dump/wp-content*.tar alexandr.koptev@gmail.com
+sleep 60
+mpack -s"" -d /sqldb_dump/wp-content${TIMESTAMP}.tar $destmail
 sleep 5
 mv /sqldb_dump/wp-content${TIMESTAMP}.tar /sqldb_dump/wp-content-old.tar
 
